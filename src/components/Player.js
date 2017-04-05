4e5bd613c12aa45924f055dd7e85335e9c29c0a1
Torben Korb <korb@a-w-media.de>
2017-04-06 00:22:18 +0200

    add eslint conf file to the app

--- src/components/Player.js
+++ src/components/Player.js
@@ -1,31 +1,31 @@
 import React, { PropTypes } from 'react';
 import Counter from './Counter';
 
-const Player = props => {
-    return (
-        <div className="player">
-                <a className="remove-player" onClick={ () => props.removePlayer(props.index) }>×</a>
-            <div className="player-name" onClick={ () => props.selectPlayer(props.index)}>
-                {props.name}
-            </div>
-            <div className="player-score">
-                <Counter
-                    index={props.index}
-                    score={props.score}
-                    updatePlayerScore={props.updatePlayerScore}
-                />
-            </div>
-        </div>
-    );
-}
+const Player = (props) => {
+  return (
+    <div className="player">
+      <a className="remove-player" onClick={ () => props.removePlayer(props.index) }>×</a>
+      <div className="player-name" onClick={ () => props.selectPlayer(props.index)}>
+        {props.name}
+      </div>
+      <div className="player-score">
+        <Counter
+        index={props.index}
+        score={props.score}
+        updatePlayerScore={props.updatePlayerScore}
+        />
+      </div>
+    </div>
+  );
+};
 
 Player.propTypes = {
-    name: PropTypes.string.isRequired,
-    index: PropTypes.number.isRequired,
-    score: PropTypes.number.isRequired,
-    removePlayer: PropTypes.func.isRequired,
-    updatePlayerScore: PropTypes.func.isRequired,
-    selectPlayer: PropTypes.func.isRequired
+  name: PropTypes.string.isRequired,
+  index: PropTypes.number.isRequired,
+  score: PropTypes.number.isRequired,
+  removePlayer: PropTypes.func.isRequired,
+  updatePlayerScore: PropTypes.func.isRequired,
+  selectPlayer: PropTypes.func.isRequired
 };
 
 export default Player;
